75bf9e09ba2e74fce8ce07b4f8f3edd9954c33e0
Music <musicm987@gmail.com>
2020-12-14 12:08:49 -0700

    created signup, login, and recording pages

---
+++ src/components/Recording.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Recording = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Recording;
+
---
+++ src/components/RecordingContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecordingContainer = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default RecordingContainer;
